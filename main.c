f539c7c60ce65f82832cce382fc28cd2d4924e3c
Lyndon Armitage <lyndon.armitage@gmail.com>
2020-06-17 11:28:15 +0100

    Add ability to choose maze algorithm from command line

--- main.c
+++ main.c
@@ -23,9 +23,41 @@ int main(int argc, char **args) {
         height = 50;
     }
 
+    if (width < 0) {
+        fprintf(stderr, "width must be greater than 0, was %d", width);
+        return EXIT_FAILURE;
+    }
+    if (height < 0) {
+        fprintf(stderr, "height must be greater than 0, was %d", height);
+        return EXIT_FAILURE;
+    }
+
+    void (*algorithm)(const Maze *);
+    if (argc >= 4) {
+        char *name = args[3];
+        if (strcmp(name, "aldous") == 0) {
+            algorithm = generate_aldous_broder_maze;
+        } else if (strcmp(name, "hunt") == 0 || strcmp(name, "kill") == 0 || strcmp(name, "huntkill") == 0) {
+            algorithm = generate_hunt_and_kill_maze;
+        } else if (strcmp(name, "sidewinder") == 0) {
+            algorithm = generate_sidewinder_maze;
+        } else if (strcmp(name, "binary") == 0 || strcmp(name, "tree") == 0) {
+            algorithm = generate_binary_tree_maze;
+        } else if (strcmp(name, "bsp") == 0) {
+            algorithm = generate_BSP_maze;
+        } else {
+            fprintf(stderr, "Unknown algorithm: %s", name);
+            return EXIT_FAILURE;
+        }
+    } else {
+        algorithm = generate_hunt_and_kill_maze;
+    }
+
+    const int cell_size = 10;
+
     srand(time(NULL));
     Maze *maze = new_maze(width, height, false);
-    render_maze_with_refresh(maze, 10, generate_BSP_maze);
+    render_maze_with_refresh(maze, cell_size, algorithm);
     delete_maze(maze);
     return 0;
 }
